9528eeaf69b3cd45dc9dad8f48de0d2c27c60230
Kim-Seong-Ho <kksshh0612@gmail.com>
2024-06-24 16:08:26 +0900

    test : 테스트 통합 추상클래스 생성

---
+++ src/test/java/NestNet/NestNetWebSite/TestSupport.java
@@ -0,0 +1,9 @@
+package NestNet.NestNetWebSite;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class TestSupport {
+}
